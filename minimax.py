540194ee203032e1b6392637e7b4ca8d527ce701
Yara Zaki <61282472+yarazakii@users.noreply.github.com>
2021-05-29 18:27:54 +0200

    update minimax.py

--- minimax.py
+++ minimax.py
@@ -38,12 +38,12 @@ class Node:
     def minimax(self):
         
         if len(self.possible_states) == 0: ### GET SCORE
-            score = self.board[6] - self.board[13]
+            score = abs(self.board[6] - self.board[13])
             return score, None
         
         if gameover(self.board):
             #### FUNCTION TO COLLECT STONES IN EACH PLAYER'S MANCALA --- RETURN BOARD ####
-            score = self.board[6] - self.board[13]
+            score = abs(self.board[6] - self.board[13])
             return score, None
             
         best_state = None
